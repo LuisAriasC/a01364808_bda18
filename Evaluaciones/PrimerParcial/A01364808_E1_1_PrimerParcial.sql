17ff4a4b86b19cf5e3784c58ddc7573705b33e48
LuisAriasC <luis.ac945@gmail.com>
2018-02-28 16:10:08 -0600

    PrimerParcial

--- Evaluaciones/PrimerParcial/A01364808_E1_1_PrimerParcial.sql
+++ Evaluaciones/PrimerParcial/A01364808_E1_1_PrimerParcial.sql
@@ -59,22 +59,16 @@ CREATE PROCEDURE update_languages()
       END IF;
 
       IF (SELECT category_id FROM film_category WHERE film_id = ids) = 6 THEN
-        # Documentary(6) then italian(2)
         UPDATE film SET original_language_id=2 WHERE film_id = ids;
       ELSEIF (SELECT category_id FROM film_category WHERE film_id = ids) = 9 THEN
-        # Foreign(9) then japanese(3)
         UPDATE film SET original_language_id=3 WHERE film_id = ids;
       ELSEIF (SELECT COUNT(*) FROM film_actor WHERE film_id = ids AND actor_id=31) = 1 THEN
-        # SOBIESKI(31) then german(6)
         UPDATE film SET original_language_id=6 WHERE film_id = ids;
       ELSEIF (SELECT COUNT(*) FROM film_actor WHERE film_id = ids AND actor_id=3) = 1 THEN
-        # ED CHASE(3) then mandarin(4)
         UPDATE film SET original_language_id=4 WHERE film_id = ids;
       ELSEIF (SELECT COUNT(*) FROM film_actor WHERE film_id = ids AND actor_id=34) = 1 THEN
-        # AUDREY OLIVIER(34) then french(5)
         UPDATE film SET original_language_id=5 WHERE film_id = ids;
       ELSE
-        # Else english then 1
         UPDATE film SET original_language_id=1 WHERE film_id = ids;
       END IF;
     END LOOP;
